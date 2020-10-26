065fd7150d4b9fb27bc86a30ee89cfea022a5566
IvanFilipov <vanaka1189@gmail.com>
2020-10-27 01:23:02 +0200

    Add last changes from the code left on our raspberry, move presentations around

--- Rpi_code/src/main.py
+++ Rpi_code/src/main.py
@@ -1,9 +1,7 @@
 import sys
 import threading
-import time
 
 sys.path.append('../../common')
-
 from constants import *
 
 from client_server.communication import \
@@ -14,12 +12,14 @@ from camera.camera_processings import Camera
 from control.robo_control import RobotController
 
 def start_http_server_thread():
-    thread = threading.Thread(target = start_HTTP_server)
+    thread = threading.Thread(target=start_HTTP_server)
     thread.daemon = True
     thread.start()
 
+
 def block_until_object_detection(cam):
     cam.detect_object_blocking()
+
 
 def capture_object_from_diff_angles(cam, robo):
     cam.store_capture(RPI_IMG_PATH_CENTER_LOCAL)
@@ -28,17 +28,17 @@ def capture_object_from_diff_angles(cam, robo):
     robo.go_to_capture_right_pos()
     cam.store_capture(RPI_IMG_PATH_RIGHT_LOCAL)
     robo.go_to_center()
-    
 
-if __name__ == "__main__":
+
+def main():
     print("Initialization...")
-    cam  = Camera()
+    cam = Camera()
     robo = RobotController()
     start_http_server_thread()
-    
+
     print("Returning to home position...")
     robo.go_to_home_pos()
-    
+
     while True:
         cam.store_capture("demo_img/begining.jpg")
         print("Detecting object...")
@@ -48,12 +48,16 @@ if __name__ == "__main__":
 
         print("Making some captures of the object...")
         capture_object_from_diff_angles(cam, robo)
-    
+
         print("Telling the PC that we are ready with image processing...")
-        #send_ready_to_PC()
-    
+        send_ready_to_PC()
+
         print("Blocking until the object is classified by the PC...")
-        #client_sem.acquire(True) # block for client request
-    
+        client_sem.acquire(True)  # block for client request
+
         print("Going to bin: ", get_last_category())
-        robo.go_to_bin_pos(4) #int(get_last_category()))
+        robo.go_to_bin_pos(int(get_last_category()))
+
+
+if __name__ == "__main__":
+    main()
